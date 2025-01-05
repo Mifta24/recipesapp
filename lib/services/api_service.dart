6cfeae47d346744c8457c80b78465047525bd9c7
Mifta Aldi <miftaadit19@gmail.com>
2025-01-05 14:49:49 +0700

    Update API service

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,15 +1,15 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:recipesapp/models/recipe_model.dart';
- // Import model yang sudah dibuat
 
-class RecipeService {
+class ApiService {
   final String apiKey = '6d702e6fc9e349ee9ac552ce6093eb5e';
   final String baseUrl = 'https://api.spoonacular.com/recipes';
 
-  /// Fetch recipes based on a query (e.g., "pasta")
-  Future<List<Recipe>> fetchRecipes(String query) async {
-    final url = Uri.parse('$baseUrl/complexSearch?query=$query&apiKey=$apiKey');
+  /// Fetch recipes based on a query
+  Future<List<Recipe>> fetchRecipes(String query, {int number = 10}) async {
+    final url = Uri.parse(
+        '$baseUrl/complexSearch?query=$query&number=$number&apiKey=$apiKey');
 
     final response = await http.get(url);
 
@@ -34,6 +34,39 @@ class RecipeService {
       return Recipe.fromJson(data);
     } else {
       throw Exception('Failed to load recipe details');
+    }
+  }
+
+  /// Fetch random recipes
+  Future<List<Recipe>> fetchRandomRecipes({int number = 5}) async {
+    final url = Uri.parse('$baseUrl/random?number=$number&apiKey=$apiKey');
+
+    final response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+      final List recipes = data['recipes'];
+
+      return recipes.map((json) => Recipe.fromJson(json)).toList();
+    } else {
+      throw Exception('Failed to load random recipes');
+    }
+  }
+
+  /// Fetch list of recipes for infinite scrolling
+  Future<List<Recipe>> fetchRecipeList({int number = 50, int offset = 0}) async {
+    final url = Uri.parse(
+        '$baseUrl/complexSearch?number=$number&offset=$offset&apiKey=$apiKey');
+
+    final response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+      final List recipes = data['results'];
+
+      return recipes.map((json) => Recipe.fromJson(json)).toList();
+    } else {
+      throw Exception('Failed to load recipe list');
     }
   }
 }
